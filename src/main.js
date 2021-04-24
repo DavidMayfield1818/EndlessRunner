e02ea09c9e5fd04e2c23b8641b6a51a9dad1bf55
David Mayfield <davidmayfield1818@yahoo.com>
2021-04-23 23:52:59 -0700

    more little changes for setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ let config = {
     type: Phaser.CANVAS,
     width: 512,     // subject to change
     height: 768,    // subject to change
+    
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: [Loading, Menu, Play01, Instructions, Credits]
 }
 
