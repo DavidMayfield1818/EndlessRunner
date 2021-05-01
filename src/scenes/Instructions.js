2dd304fb7e3db815ab43ee6dca97eb18374d1907
David Mayfield <davidmayfield1818@yahoo.com>
2021-05-01 03:10:34 -0700

    more instruction updates

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -23,14 +23,16 @@ class Instructions extends Phaser.Scene {
         menuConfig.fontSize = '17px';
         menuConfig.backgroundColor = '#000000';
         menuConfig.color = '#FFFFFF';
-        this.add.text(game.config.width/2, game.config.height/2, 'Click to kick the ball', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 32, 'The further you click the farther it goes', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 64, 'Kick to your teammates to earn points', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - 32, 'Click to kick the ball', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'The further you click the farther it goes', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 32, 'Kick to your teammates to earn points', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 + 96, 'You can only kick if your temmate has the ball', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 128, 'Every kick increases your streak', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 160, 'The streak points are worth more and more', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 192, 'Enemy player are -5 points and reset the streak', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 224, 'The blackhole means game over', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 64, 'Every kick increases your streak', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 128, 'The streak points are worth more and more', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 160, 'Enemy player are -5 points and reset the streak', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 192, 'Enemy players can still kick', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 224, 'So use them to stay alive if you need to', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 256, 'The blackhole means game over', menuConfig).setOrigin(0.5);
 
         let backButton = this.add.text(game.config.width - 128, game.config.height - 64, 'Menu', menuConfig).setOrigin(0.5);
         
